894d7245f9ba07fa7dd4b2a4156fda57eeac1e55
badrinath <pallam.badrinath@gmail.com>
2022-03-21 11:24:23 +0530

    basic lumen api project

---
+++ lumen/database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'name' => 'badri',
+            'price' => '500',
+            'description' => 'badri',
+        ]);
+    }
+}
